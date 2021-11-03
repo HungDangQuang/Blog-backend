86503d1f58cf1dc3ab8526727e3a72acd4cc6972
HungDangQuang <hungdangkg@gmail.com>
2021-11-03 15:14:28 +0700

    Set up body-parser and cors

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-var express = require('express')
+const bodyParser = require('body-parser')
+const express = require('express')
+const cors = require('cors')
 
 var app = express()
 
 var PORT = process.env.PORT || 3000
 
+app.use(cors())
+
+// Configure body-parser middleware
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json)
+
 app.listen(PORT, ()=>{
     console.log('Server is listening on  this port:', PORT )
 })
+
 
 app.get('/', (req,res)=>{
     res.send("Hello, this is a blog website")
