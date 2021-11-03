c80ea02914708dab6eb1bab799bd1261fb8f86c5
HungDangQuang <hungdangkg@gmail.com>
2021-11-03 15:24:49 +0700

    Set up mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 var app = express()
 
 var PORT = process.env.PORT || 3000
+
+dotenv.config()
 
 app.use(cors())
 
@@ -16,6 +20,14 @@ app.listen(PORT, ()=>{
     console.log('Server is listening on  this port:', PORT )
 })
 
+mongoose.connect(process.env.DB_CONN, (err)=>{
+    if(err){
+        console.log('Connection failed with err ', err)
+    }
+    else {
+        console.log('Connected to mongoDB')
+    }
+})
 
 app.get('/', (req,res)=>{
     res.send("Hello, this is a blog website")
