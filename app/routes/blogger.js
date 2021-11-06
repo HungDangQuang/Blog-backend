57f32499d7173d8e614dd6bcfc63fbb1840563cc
HungDangQuang <hungdangkg@gmail.com>
2021-11-06 19:24:06 +0700

    make it better

--- app/routes/blogger.js
+++ app/routes/blogger.js
@@ -3,4 +3,7 @@ const {register,login} = require('../controller/bloggerCtrl')
 module.exports = function(app){
     app.post('/register', register)
     app.post('/login', login)
+    app.get('/test',function(req,res){
+        res.send('hello')
+    })
 }
