53fed44cf398622d991513bc3ad627e138f5594d
HungDangQuang <hungdangkg@gmail.com>
2021-11-06 18:34:09 +0700

    make it better

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express()
 
 dotenv.config()
 app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Server is listening on this port:', PORT )
+    console.log('Server is listening on this port' )
 })
 app.use(cors())
 
