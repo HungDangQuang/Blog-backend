c35593f6bf14be48b48075d53cfbe477529a2048
HungDangQuang <hungdangkg@gmail.com>
2021-11-06 19:10:42 +0700

    set up header

--- server.js
+++ server.js
@@ -13,6 +13,13 @@ app.listen(process.env.PORT || 3000, ()=>{
 })
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // Configure body-parser middleware
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
