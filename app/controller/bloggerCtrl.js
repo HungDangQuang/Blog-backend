02bf33087885f0cd19f9e7bf1341b2a70d6c28e0
HungDangQuang <hungdangkg@gmail.com>
2021-11-06 23:03:25 +0700

    ...

--- app/controller/bloggerCtrl.js
+++ app/controller/bloggerCtrl.js
@@ -7,39 +7,40 @@ dotenv.config()
 
 module.exports = {
     register: async(req,res,next)=>{
-        try {
+        // try {
 
-            // Get User Info
-            const username = req.body.username
-            const email = req.body.email
-            const password = req.body.password
+        //     // Get User Info
+        //     const username = req.body.username
+        //     const email = req.body.email
+        //     const password = req.body.password
 
-            // check wether email or passsword is empty
-            if(!email || !password || !username){
-                return res.status(400).json({message:"Email or Password or Username Must Not Be Empty"})
+        //     // check wether email or passsword is empty
+        //     if(!email || !password || !username){
+        //         return res.status(400).json({message:"Email or Password or Username Must Not Be Empty"})
 
-            }
+        //     }
 
-            // check wether email is duplicated
-            const query = await Blogger.findOne({email})
-            if(query){
-                return res.status(400).json({message:"This Email Has Already Used"})
-            }
-            // hash password
-            const hashPass = await bcrypt.hash(password,10)
+        //     // check wether email is duplicated
+        //     const query = await Blogger.findOne({email})
+        //     if(query){
+        //         return res.status(400).json({message:"This Email Has Already Used"})
+        //     }
+        //     // hash password
+        //     const hashPass = await bcrypt.hash(password,10)
 
-            // create new Blogger
-            const newBlogger = new Blogger({username,email,password:hashPass})
+        //     // create new Blogger
+        //     const newBlogger = new Blogger({username,email,password:hashPass})
 
-            // save blogger
-            await newBlogger.save()
+        //     // save blogger
+        //     await newBlogger.save()
 
-            // return new blogger
-            return res.status(201).json({user:newBlogger, message:'Create new user successfully'})
-        }
-        catch(err){
-            next(err)
-        }
+        //     // return new blogger
+        //     return res.status(201).json({user:newBlogger, message:'Create new user successfully'})
+        // }
+        // catch(err){
+        //     next(err)
+        // }
+        res.send("heelo")
     },
 
     login: async(req,res,next)=>{
